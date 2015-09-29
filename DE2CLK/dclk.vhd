fe8824dd226f3dce06c77c3e88c6da40c192cd88
XHelaoshi <hhy132831@126.com>
2015-09-29 14:12:34 +0800

    正在加入日月

--- DE2CLK/dclk.vhd
+++ DE2CLK/dclk.vhd
@@ -47,15 +47,70 @@ begin
 			ss <= s0;
 		elsif clk'event and clk = '1' then
 			case ss is 
-				when s0 =>
-					
-				when s1 =>
-				
+				when s0 => -- for 31days' month
+					if monthcnth = 0 then
+						if monthcntl = 1 or monthcntl = 3 or monthcntl = 5 or monthcntl = 7 or monthcntl = 8 then
+							ss <= s1;
+						end if;
+					else 
+						if monthcntl = 0 or monthcntl = 2 then
+							ss <= s1;
+						end if;
+					end if;
+				when s1 => --for 30days' month
+					if monthcnth = 0 then
+						if monthcntl = 2 or monthcntl = 4 or monthcntl = 6 or monthcntl = 9 then
+							ss <= s0;
+						end if;
+					else 
+						if monthcntl = 1 then
+							ss <= s0;
+						end if;
+					end if;
 				when others =>
-				
+					ss <= s0;
 			end case;
 		end if;
 	end process;
+	
+	process(rst,clk)
+	begin
+	if rst = '1' then
+		monthcntl <= "1";
+		monthcnth <= "0";
+		daycntl <= "1";
+		daycnth <= "0";
+	elsif clk'event and clk = '1' then
+		if ss = s0 then --31days
+			if daycnth = 3 and daycntl = 1 then
+				daycntl <= 1;
+				daycnth <= 0;
+				monthcntl <= monthl + 1;
+			elsif daycntl = 9 then
+				daycnth <= daycnth + 1;
+				daycntl <= 0;
+			else
+				daycntl <= daycntl + 1;
+			end if;
+			if monthcnth = 1 and monthcntl = 2 then
+				monthcnth <= 0;
+				monthcntl <= 1;
+			end if;
+		elsif ss = s1 then--30days
+			if daycnth = 3 and daycntl = 0 then
+				daycntl <= 1;
+				daycnth <= 0;
+				monthl <= monthl + 1;
+			elsif daycntl = 9 then
+				daycnth <= daycnth + 1;
+				daycntl <= 0;
+			else
+				daycntl <= daycntl + 1;
+			end if;
+		end if;
+	end if;
+	end process;
+	
 	
 	process(rst,clk)
 	begin
